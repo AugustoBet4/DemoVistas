efb3babd3eaa1922fcae1e4a491aec0bd8aa5673
AugustoBet4 <augusto.bet4@gmail.com>
2016-11-30 16:54:31 -0400

    Initial commit

---
+++ app/src/main/java/bo/pochoclos/com/vistas/MainActivity.java
@@ -0,0 +1,13 @@
+package bo.pochoclos.com.vistas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;x
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
